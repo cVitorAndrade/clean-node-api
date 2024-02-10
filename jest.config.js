572308188198836016abb5ce421c2c4a1835f9d3
cVitorAndrade <vitorandrade1804@gmail.com>
2024-02-10 17:38:24 -0300

    chore: add jest and configure husky

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+module.exports = config
